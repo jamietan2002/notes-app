49be8cc4b44dc87d70b1acc69a048af7d9488da1
jamietan2002 <jamietan2002@gmail.com>
2023-12-19 04:06:26 -0500

    User Interface home page

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,119 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useState } from 'react';
 
-const Note = ({ title, content }) => {
-    return (
-        <Box sx={{ padding: "16px", borderRadius: 4, border: "1px solid #ddd", marginBottom: 16 }}>
-            <Typography variant="h6" fontWeight="bold">Note by: {title}</Typography>
-            <Typography variant="body1">Summary: {content}</Typography>
-        </Box>
-    );
+
+// const Note = ({ title, summarised, author, createdDate, tag, content }) => {
+//     return (
+//         <Box sx={{ padding: "16px", borderRadius: 4, border: "1px solid #ddd", marginBottom: 16 }}>
+//             <Typography variant="h6" fontWeight="bold">Note by: {title}</Typography>
+//             <Typography variant="body1">
+//         <ul>
+//           {Object.entries(summarised).map(([key, value]) => (
+//             <li key={key}>
+//               <strong>{key}:</strong> {value}
+//             </li>
+//           ))}
+//         </ul>
+//       </Typography>
+//         </Box>
+//     );
+// };
+
+// export default Note;
+
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+
+const Note = ({ title, summarised, author, createdDate, tag, content }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  console.log(createdDate)
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            M
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={title}
+  subheader={
+    <>
+      {createdDate.toDate().toDateString()} by {author.toUpperCase()}
+    </>
+  }
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.primary">
+          <ul>
+          {Object.entries(summarised).map(([key, value]) => (
+            <li key={key}>
+              <strong>{key}:</strong> {value}
+            </li>
+          ))}
+        </ul>
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Full Note:</Typography>
+          <Typography paragraph>
+            {content}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 };
 
 export default Note;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,19 +8,38 @@ import Header from '../components/nav/Header'
 import { Box, Container, Typography, Grid, Button } from "@mui/material";
 import Note from "../components/Note";
 import createNote from '../functions/createNote';
+import { getAuth } from "firebase/auth";
 
 
 const Home = () => {
     const collectionRef = collection(FIREBASE_DB, 'notes')
     const [notes, setNotes] = useState([]);
+    const auth = getAuth();
+    // const user = auth.currentUser;
+
+    // if (user) {
+    //     console.log(user.email)
+    // } else {
+    //     console.log("user not signed in")
+    // }
 
 
     useEffect(() => {
+        const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+        console.log(user.email)
+    } else {
+        console.log("user not signed in")
+    }
         const getNotes = async () => {
             const q = query(collectionRef)
             await getDocs(q).then((note) => {
                 let noteData = note.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+                noteData = noteData.filter((note) => note.tag.includes(user.email));
                 setNotes(noteData)
+                console.log(noteData)
 
             }).catch((err) => {
                 console.log(err);
@@ -57,7 +76,7 @@ const Home = () => {
                         {/* Notes list */}
                         {notes.map((note) => (
                             <Grid item xs={12} md={6} key={note.title}>
-                                <Note title={note.author} content={note.content} />
+                                <Note title={note.title} summarised={note.summarised} />
                             </Grid>
                         ))}
                     </Grid>
