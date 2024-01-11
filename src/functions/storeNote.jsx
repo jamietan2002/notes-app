91a674b64be8a4d8ce107390989afccd9988e1aa
jamietan2002 <jamietan2002@gmail.com>
2024-01-11 03:30:35 -0500

    Update error page

--- src/functions/storeNote.jsx
+++ src/functions/storeNote.jsx
@@ -10,29 +10,6 @@ import {
 const notesRef = doc(collection(FIREBASE_DB, "notes"));
 
 const storeNote = async (toStore) => {
-  // let summary = toStore.summarised;
-  // summary = summary.trim();
-  // const lines = summary.split("\n");
-  // const summaryMap = {};
-  // for (const line of lines) {
-  //   console.log(line);
-  //   if (line.trim() === "") {
-  //     continue;
-  //   }
-  //   const [name, text] = line.split(": ", 2);
-  //   const trimmedName = name.trim().replace(/^\d+\.\s*/, "");
-
-  //   summaryMap[trimmedName] = text.trim();
-  // }
-
-  // let createdNote = {
-  //   title: toStore.title,
-  //   content: toStore.content,
-  //   tags: toStore.tags,
-  //   author: toStore.author,
-  //   summarised: summaryMap,
-  // };
-
   if (toStore.id) {
     // Update existing document
     await updateDoc(doc(FIREBASE_DB, "notes", toStore.id), toStore);
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -5,6 +5,7 @@ const ErrorPage = () => {
     <div>
       <h1>Oops, something went wrong!</h1>
       <p>We're working hard to fix the issue. Please try again later.</p>
+      <a href="/login">Go back to home page</a>
     </div>
   );
 };
